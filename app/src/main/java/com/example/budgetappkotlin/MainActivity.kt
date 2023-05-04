527509729cfea60c15353460ed4c49cbcd8c51b6
Ahaan25 <ahaan25092002@gmail.com>
2023-05-05 01:45:28 +0530

    Added theme switch

--- app/src/main/java/com/example/budgetappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/budgetappkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.budgetappkotlin
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Switch
@@ -13,20 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedPreferences=getSharedPreferences("sp", MODE_PRIVATE)
-        val editor=sharedPreferences.edit()
         val switch:Switch=findViewById(R.id.switch1)
+
+        val sharedPreferences=getSharedPreferences("theme", Context.MODE_PRIVATE)
+        val editor=sharedPreferences.edit()
+        val nightMode=sharedPreferences.getBoolean("night", false)
+
+        if(nightMode){
+            switch.isChecked=true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
 
         switch.setOnCheckedChangeListener{ buttonView, isChecked ->
             if(isChecked){
                 AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-                editor.putBoolean("isDarkTheme", true)
+                editor.putBoolean("night", true)
                 editor.apply()
                 Toast.makeText(this@MainActivity, "Dark theme", Toast.LENGTH_SHORT).show()
             }
             else{
                 AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
-                editor.putBoolean("isDarkTheme", false)
+                editor.putBoolean("night", false)
                 editor.apply()
                 Toast.makeText(this@MainActivity, "Light theme", Toast.LENGTH_SHORT).show()
             }
